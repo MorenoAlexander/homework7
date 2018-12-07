be0104628e2ab4a6c0879278e121170bd9782973
MorenoAlexander <amoren26@uic.edu>
2018-12-06 23:11:13 -0600

    adding basic time instrumentations to macro_processes

--- macro_processes.c
+++ macro_processes.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include "statistics.h"
 
 #define NUMTASKS 25000
 #define NANOSECONDS_PER_SECOND 1E9
@@ -18,12 +19,26 @@ int fib(int x) {
 }
 
 int main(){
+
+  StatObject procSpawn = NewStatObject();
+
+
+  struct timespec spawnStart;
+  struct timespec spawnAfter;
   int wpid, status, retval;
   
+
+
+  
   for (int i = 0; i < NUMTASKS; i++) {
+    clock_gettime(CLOCK_REALTIME,&spawnStart);
     retval = fork();
+    clock_gettime(CLOCK_REALTIME,&spawnAfter);
+    double difference = (spawnAfter.tv_sec * NANOSECONDS_PER_SECOND + spawnAfter.tv_nsec) - (spawnStart.tv_sec * NANOSECONDS_PER_SECOND + spawnStart.tv_nsec);
+    stat_object_add(procSpawn,difference);
     if (retval == -1){
       // over our process limit: wait for at least one process to finish and try again
+      //difference = 0;
       wpid = wait(&status);
       if (wpid < 0){
         printf("waited when no children to wait for\n");
@@ -34,6 +49,7 @@ int main(){
       continue;
     }
     if (retval == 0){
+
       fib(FIBNUM);
       exit(0);
     }
@@ -43,5 +59,10 @@ int main(){
   }
   // wait for all worker processes to finish
   while ((wpid = wait(&status)) > 0);
+
+  StatResult r = (StatResult)malloc(sizeof(sStatResult));
+  stat_obj_value(procSpawn, r);
+  printf("time to spawn a process: avg: %10.4f min: %10.4f max: %10.4f stddev: %10.4f\n",
+         r->mean, r->min, r->max, r->stddev);
   return 0;
 }
